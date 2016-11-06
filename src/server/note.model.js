acb821650e3d5946563ecd01e0f8c4967c3eb0df
Stephen Sullivan <sksulli2@illinois.edu>
2016-11-06 01:45:06 -0600

    Added dates to note model

--- src/server/note.model.js
+++ src/server/note.model.js
@@ -4,8 +4,10 @@ const Schema = mongoose.Schema;
 const TagSchema = mongoose.model('Tag').schema;
 
 const NoteSchema = new Schema({
-  title: { type: String, default: '' },
-  text: { type: String, default: '' },
+  title: { type: String, default: '', required: true },
+  text: { type: String, default: '', required: true },
+  dateCreated: { type: Date, default: Date.now, required: true },
+  dateEdited: { type: Date, default: Date.now, required: false },
   tags: [TagSchema]
 });
 
